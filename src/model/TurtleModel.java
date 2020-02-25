7ac279fb242466aa248c903bfc3261beabcb7de1
Eric Jiang <edj9@duke.edu>
2020-02-24 21:39:30 -0500

    switching to eric branch

--- src/model/TurtleModel.java
+++ src/model/TurtleModel.java
@@ -4,6 +4,9 @@ public class TurtleModel {
   private double myX;
   private double myY;
   private double myAngle;
-  public TurtleModel () {
+  public TurtleModel (double myX, double myY, double myAngle) {
+    this.myX = myX;
+    this.myY = myY;
+    this.myAngle = myAngle;
   }
 }

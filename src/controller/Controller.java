0055f94edda5af764f4c3f8f49efc3647a627ea6
Abhijay Suhag <as866@duke.edu>
2020-02-25 21:36:54 -0500

    beginning work on controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,20 @@
 package controller;
 
-public class Controller {
-    public Controller() {
+import execution.CommandFactory;
+import model.TurtleModel;
+import parsing.Parser;
+import view.TurtleView;
 
+public class Controller {
+    private TurtleModel turtleModel;
+    private TurtleView turtleView;
+    private Parser parser;
+    private CommandFactory commandFactory;
+
+    public Controller() {
+        this.turtleView = new TurtleView();
+        this.commandFactory = new CommandFactory();
+        //this.turtleModel = new TurtleModel()
     }
-    
+
 }

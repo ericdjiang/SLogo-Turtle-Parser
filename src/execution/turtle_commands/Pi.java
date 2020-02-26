e504982fa9adbe679282119336326997d53e4fa9
Doherty Guirand <dg211@duke.edu>
2020-02-25 21:01:16 -0500

    adding math commands

--- src/execution/turtle_commands/Pi.java
+++ src/execution/turtle_commands/Pi.java
@@ -5,15 +5,14 @@ import model.TurtleModel;
 
 import java.util.List;
 
-public class SetTowards implements Command {
+public class Pi implements Command {
     @Override
     public double execute(List<Double> parameters, TurtleModel turtleModel) {
-        System.out.println("getting number of degrees turned" + parameters.get(0));
-        return parameters.get(0);
+        return Math.PI;
     }
 
     @Override
     public int getNumParams() {
-        return 1;
+        return 0;
     }
 }
--- src/execution/turtle_commands/Random.java
+++ src/execution/turtle_commands/Random.java
@@ -5,11 +5,12 @@ import model.TurtleModel;
 
 import java.util.List;
 
-public class SetTowards implements Command {
+public class Random implements Command {
     @Override
     public double execute(List<Double> parameters, TurtleModel turtleModel) {
-        System.out.println("getting number of degrees turned" + parameters.get(0));
-        return parameters.get(0);
+        double randomnum = Math.random() * parameters.get(0) + 1;
+        System.out.println("Inside Random Class: Ramdom number is " + (int) randomnum + ": param is " + parameters.get(0));
+        return randomnum;
     }
 
     @Override

f8be586eb65a6de23de0c84d7ebcbc7e0d36b5d3
Doherty Guirand <dg211@duke.edu>
2020-02-25 22:05:58 -0500

    fixed null pointer exception

---
+++ src/execution/boolean_operations/Or.java
@@ -0,0 +1,23 @@
+package execution.boolean_operations;
+
+import execution.Command;
+import model.TurtleModel;
+
+import java.util.List;
+
+public class Or implements Command {
+    @Override
+    public double execute(List<Double> parameters, TurtleModel turtleModel) {
+        if(parameters.get(0) != 0 || parameters.get(1) != 0){
+            System.out.println(1);
+            return 1;
+        }
+        System.out.println(0);
+        return 0;
+    }
+
+    @Override
+    public int getNumParams() {
+        return 2;
+    }
+}

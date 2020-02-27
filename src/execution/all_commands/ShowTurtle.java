0b91d08069ce59854e9ca2af6e5e1adab433d1dc
Doherty Guirand <dg211@duke.edu>
2020-02-26 20:11:12 -0500

    finished turtle commands

--- src/execution/all_commands/ShowTurtle.java
+++ src/execution/all_commands/ShowTurtle.java
@@ -1,4 +1,4 @@
-package execution.math_operations;
+package execution.all_commands;
 
 import execution.Command;
 import model.ConsoleModel;
@@ -7,16 +7,15 @@ import model.VariableModel;
 
 import java.util.List;
 
-public class Sum implements Command {
-
+public class ShowTurtle implements Command {
     @Override
     public double execute(List<String> parameters, TurtleModel turtleModel, VariableModel variableModel, ConsoleModel consoleModel) {
-        double sum = Double.parseDouble(parameters.get(0)) + Double.parseDouble(parameters.get(1));
-        return sum;
+        turtleModel.showTurtle();
+        return 1;
     }
 
     @Override
     public int getNumParams() {
-        return 2;
+        return 0;
     }
 }

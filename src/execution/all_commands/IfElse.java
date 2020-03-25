e6a5858c22b407db63741c369d96060203e8a9ba
Eric Jiang <edj9@duke.edu>
2020-03-25 01:10:27 -0400

    added comments to masterpiece

--- src/execution/all_commands/IfElse.java
+++ src/execution/all_commands/IfElse.java
@@ -10,6 +10,14 @@ import model.*;
 import parsing.Parser;
 
 public class IfElse extends LoopCommand implements Command {
+
+  /***
+   * Parses the if loop guard and executes either the if/else body based on the loop guard output
+   * @param parameters a list of each parameter as a space delimited string
+   * @param TurtleModel the turtle model containing position states/methods
+   * @param allModels a wrapper class containing user defined methods and variables classes
+   * @return the last return value of the parsed loop body
+   */
   @Override
   public double execute(List<String> parameters, TurtleModel TurtleModel, ModelContainer allModels){
     List <String> symbolList = Arrays.asList(parameters.get(0).split("[ ]+"));
@@ -20,24 +28,22 @@ public class IfElse extends LoopCommand implements Command {
     Parser loopGuardParser = new Parser(String.join(" ", symbolList.subList(1, expEnd)), language, allModels);
     int loopGuard = (int) Math.round(loopGuardParser.getLastReturnValue());
 
-
     String [] loopBodies = getLoopBodies(symbolList);
 
-//    System.out.println(language);
-//    System.out.println(numRepeats);
-//    System.out.println(loopBody);
-
+    Parser parser;
     if (loopGuard > 0){
-       Parser parser = new Parser(loopBodies[0], language, allModels);
+       parser = new Parser(loopBodies[0], language, allModels);
     } else {
       System.out.println(loopBodies[1]);
-      Parser parser = new Parser(loopBodies[1], language, allModels );
+      parser = new Parser(loopBodies[1], language, allModels );
     }
 
-
-    return 0;
+    return parser.getLastReturnValue();
   }
 
+  /**
+   * @return the number of parameters required
+   */
   @Override
   public int getNumParams() {
     return 1;
